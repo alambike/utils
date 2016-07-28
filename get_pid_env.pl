99422f92acfaf5a83cab948e8eb4ad56341e7f31
root <root@mukhas.com>
2016-07-28 03:14:01 +0200

    Corrected script to allow pass cmd name, and print cmdline

--- get_pid_env.pl
+++ get_pid_env.pl
@@ -3,8 +3,49 @@ use strict;
 use Data::Dumper;
 
 my $pid = $ARGV[0];
+my @pids = ();
 
-open my $f, "/proc/$pid/environ" || die($!);
+if($pid =~ /^\d+$/){
+    @pids = ($pid);
+}
+else{
+    @pids = `pgrep $pid`;
+    chomp(@pids);
+}
 
-my $linea = <$f>;
-print Dumper(split '\0', $linea);
+foreach my $pid (@pids){
+    print '-'x80, "\n";
+    print "Environment for pid $pid (cmdline='".&getcmdline($pid)."'):\n";
+    print '-'x80, "\n";
+    print_pid_env($pid);
+}
+
+
+sub getcmdline{
+    my $pid = shift;
+    return join ' ', __getProcFileContents($pid, 'cmdline');
+}
+
+sub print_pid_env{
+    my $pid = shift;
+
+    map {
+        my ($var, $value) = (split /\=/, $_);
+        print "$var = $value \n";
+    } sort {
+        $a cmp $b
+    }
+    __getProcFileContents($pid, 'environ');
+}
+
+sub __getProcFileContents{
+    my ($pid,$file) = @_;
+
+    open my $f, "/proc/$pid/$file" || die($!);
+
+    my $linea = <$f>;
+
+    chomp($linea);
+
+    return split(/\0/, $linea);
+}
