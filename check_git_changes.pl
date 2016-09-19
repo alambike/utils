2acfa870a182f6df1b9e8c66ec883e1374eb2f11
root <root@mukhas.com>
2016-09-19 13:18:21 +0200

    Script para chequear cambios en un repo de git

---
+++ check_git_changes.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+
+my $opts = &parse_command_line;
+
+my $path = $opts->{path} || die("Falta la ruta del repositorio que chequear");
+my $pattern = $opts->{pattern} || '\.php$';
+my $telegram_bot_auth = $opts->{telegram_bot_auth} || die("Falta la autenticacion del bot de telegram");
+
+my $pattern = qr/$pattern/m;
+
+my $status = &git_status($path);
+
+if(&match_status($status, $pattern)){
+
+	&notify($status,$telegram_bot_auth);
+}
+
+
+sub parse_command_line{
+
+	my $res = {};
+
+	foreach my $argv (@ARGV){
+
+		my($k, $v) = split /\=/, $argv,2;
+
+		$res->{$k} = $v;
+	}
+
+	$res;
+
+
+}
+
+sub git_status{
+
+	`cd $path && git status`
+}
+
+sub match_status {
+	my ($status, $pattern) = @_;
+
+	return $status =~ $pattern
+}
+
+sub notify{
+	my ($status) = @_;
+
+	$status =~ s/\"/\\"/g;
+
+my $cmd = 'curl -X POST https://api.telegram.org/bot148573770:AAHj4JcFcvKpGtUS8hUtxP0tW7Zmmy-5qJ4/sendMessage \
+-H "Content-Type: application/json" \
+-d \'{"chat_id":"2129731","text":"Alerta!!!\n Archivos sospeitosos en mukhas.com\n '.$status.'"}\'';
+
+print `$cmd`;
+}
